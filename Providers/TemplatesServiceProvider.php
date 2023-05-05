edd52902a112993a3b783db5e68226e4d407edbb
Ahmed Gad <eng.ahmedmgad@gmail.com>
2023-05-06 00:17:06 +0300

    update

--- Providers/TemplatesServiceProvider.php
+++ Providers/TemplatesServiceProvider.php
@@ -50,7 +50,7 @@ class TemplatesServiceProvider extends ServiceProvider
     {
         $this->publishes([
             module_path($this->moduleName, 'dist/build-templates') => public_path(),
-        ], 'assets');
+        ], 'modules-assets');
     }
 
 
